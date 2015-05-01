f4be7bbe67d422b1c9ea826f9964af004ebb6434
Truyen Nguyen <truyen.m.nguyen@gmail.com>
2015-05-01 14:10:01 -0700

    Restructure the project

--- bitmapTransform.js
+++ bitmapTransform.js
@@ -1,28 +1,17 @@
 'use strict';
 var fs = require('fs');
+var createInvertColors = require('./createInvertColorsBitmap');
+var BitmapData = require('./bitmapData');
 
 var readBitmapFile = function(filename){
     fs.readFile(filename, function(err, buffer){
         if (err) throw err;
 
-        console.log(buffer[10]);
+        //Initialize the bitmap data
+        var bitmapData = new BitmapData(buffer);
 
-        var sizeBitmapFile = buffer.readUInt32LE(2);
-        var startPixelsArray = buffer.readUInt32LE(10);
-        var numColorsInPalete = buffer.readUInt32LE(46);
-
-        var startPaletteArray = startPixelsArray - (numColorsInPalete*4);
-
-        console.log(sizeBitmapFile);
-        console.log(startPixelsArray);
-        console.log(numColorsInPalete);
-
-        //for(var i = startPixelsArray; i <= startPixelsArray + numColorsInPalete; i++){
-        for(var i = startPaletteArray; i <= startPixelsArray; i++){
-            buffer[i] = 255 - buffer[i];
-        }
-
-        fs.writeFile('./invertColor.bmp', buffer);
+        //Start the bitmap transformations
+        createInvertColors(buffer, bitmapData)
     });
 }
 
